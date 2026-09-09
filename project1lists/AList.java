8c247843139c5cff766845342aa02da07f39cee7
redox7500 <redox7500@gmail.com>
2026-09-08 19:11:06 -0700

    Done formatting List and AList hopefully, but the comments are probably in need of changing

--- project1lists/AList.java
+++ project1lists/AList.java
@@ -3,83 +3,77 @@ package project1lists;
 /** Array-based list implementation */
 class AList<E> implements List<E>
 {
-    private static final int defaultSize = 10; // Default size
-    private int maxSize; // Maximum size of list
-    private int listSize; // Number of list items now
-    private int curr; // Position current element
-    private E[] listArray; // Array holding list elements
+    /** Maximum amount of elements AList is instantiated to contain by default */
+    private static final int defaultSize = 10;
 
-    /** Constructors */
-    /** Create a list with the default capacity. */
-    AList() {this(defaultSize);}
+    /** Number of elements this instance has */
+    private int elementCount;
+
+    /** Index of the current element */
+    private int currentIndex;
+
+    /** Array holding this instance's data */
+    private E[] array;
 
     /** Create a new list object.
-        @param size Max number of elements list can contain. */
-    @SuppressWarnings("unchecked") // Generic array allocation
+     * @param size Maximum amount of elements this instance should have
+     */
+    @SuppressWarnings("unchecked") // For generic array allocation
     AList(int size)
     {
-        maxSize = size;
-        listSize = curr = 0;
-        listArray = (E[])new Object[size]; // Create listArray
+        this.elementCount = 0;
+        this.currentIndex = 0;
+        this.array = (E[])new Object[size];
     }
 
-    public void clear() /** Reinitialize the list */
-    {listSize = curr = 0;} // Simply reinitialize values
+    /** Create a list with the default capacity. */
+    AList() {this(AList.defaultSize);}
 
-    /** Insert "it" at current position */
-    public void insert(E it)
+    public int length() {return this.elementCount;}
+    
+    public int getCurrentIndex() {return this.currentIndex;}
+
+    public void setCurrentIndex(int index)
     {
-        assert listSize < maxSize : "List capacity exceeded";
-        for (int i = listSize; i > curr; i--) listArray[i] = listArray[i - 1]; // Shift elements up to make room
-        listArray[curr] = it;
-        listSize++; // Increment list size
+        assert index >= 0 && index < this.elementCount : "Pos out of range";
+        this.currentIndex = index;
     }
 
-    /** Append "it" */
-    public void append(E it)
+    public E getCurrentElement() {return this.array[this.currentIndex];}
+
+    public void setCurrentElement(E element) {this.array[this.currentIndex] = element;}
+
+    public void clear() {this.elementCount = 0; this.currentIndex = 0;}
+
+    public void insert(E element)
     {
-        assert listSize < maxSize : "List capacity exceeded";
-        listArray[listSize++] = it;
+        assert this.elementCount + 1 < this.array.length : "List capacity exceeded";
+
+        for (int i = this.elementCount; i > this.currentIndex; i--) this.array[i] = this.array[i - 1]; // Shift elements up to make room
+        this.array[this.currentIndex] = element;
+        this.elementCount++;
     }
 
-    /** Reset position */
-    public void moveToStart() {curr = 0;}
+    public void append(E element)
+    {
+        assert this.elementCount + 1 < this.array.length : "List capacity exceeded";
 
-    /** Remove and return the current element. */
+        this.array[this.elementCount++] = element;
+    }
+    
+    public void moveToStart() {this.currentIndex = 0;}
+
     public E remove()
     {
-        if ((curr<0) || (curr>=listSize)) return null; // No current element
-        E it = listArray[curr]; // Copy the element
-        for(int i=curr; i<listSize-1; i++) // Shift them down
-        listArray[i] = listArray[i+1];
-        listSize--; // Decrement size
-        return it;
+        E element = this.array[this.currentIndex]; // Copy the element
+        for (int i = this.currentIndex; i < this.elementCount - 1; i++) this.array[i] = this.array[i + 1]; // Shift them down
+        this.elementCount--; // Decrement size
+        return element;
     }
 
-    /** Reset */
-    public void moveToEnd() {curr = listSize;}
+    public void moveToEnd() {this.currentIndex = this.elementCount - 1;}
 
-    /** Back up */
-    public void prev() {if (curr != 0) curr--;}
-
-    /** Next */
-    public void next() {if (curr < listSize) curr++;}
-
-    /** Return list size */
-    public int length() {return listSize;}
-
-    /** Return current position */
-    public int currPos() {return curr;}
-
-    /** Set current list position to "pos" */
-    public void moveToPos(int pos) {
-        assert (pos >= 0) && (pos <= listSize) : "Pos out of range";
-        curr = pos;
-    }
-
-    /** Return current element */
-    public E getValue() {
-        assert (curr >= 0) && (curr < listSize) : "No current element";
-        return listArray[curr];
-    }
+    public void prev() {if (this.currentIndex != 0) this.currentIndex--;}
+    
+    public void next() {if (this.currentIndex + 1 < this.elementCount) this.currentIndex++;}
 }
--- project1lists/List.java
+++ project1lists/List.java
@@ -3,6 +3,22 @@ package project1lists;
 /** List ADT */
 public interface List<E>
 {
+    /** @return The number of elements in the list. */
+    public int length();
+
+    /** @return The index of the current element. */
+    public int getCurrentIndex();
+
+    /** Set current index.
+        @param pos The index to make current. */
+    public void setCurrentIndex(int index);
+
+    /** @return The current element. */
+    public E getCurrentElement();
+
+    /** @param The element to set the value at currentIndex to. */
+    public void setCurrentElement(E element);
+
     /** Remove all contents from the list, so it is once again
         empty. Client is responsible for reclaiming storage
         used by the list elements. */
@@ -12,43 +28,29 @@ public interface List<E>
         is responsible for ensuring that the list’s capacity
         is not exceeded.
         @param item The element to be inserted. */
-    public void insert(E item);
+    public void insert(E element);
 
     /** Append an element at the end of the list. The client
         is responsible for ensuring that the list’s capacity
         is not exceeded.
         @param item The element to be appended. */
-    public void append(E item);
+    public void append(E element);
 
     /** Remove and return the current element.
         @return The element that was removed. */
     public E remove();
 
-    /** Set the current position to the start of the list */
+    /** Set the current index to the start of the list */
     public void moveToStart();
 
-    /** Set the current position to the end of the list */
+    /** Set the current index to the end of the list */
     public void moveToEnd();
 
-    /** Move the current position one step left. No change
+    /** Move the current index one step left. No change
         if already at beginning. */
     public void prev();
 
-    /** Move the current position one step right. No change
+    /** Move the current index one step right. No change
         if already at end. */
     public void next();
-
-    /** @return The number of elements in the list. */
-    public int length();
-
-    /** @return The position of the current element. */
-    public int currPos();
-
-    /** Set current position.
-        @param pos The position to make current. */
-    public void moveToPos(int pos);
-
-    /** @return The current element. */
-    public E getValue();
-
 }
