9f0c0f7ab0280d62ae7f273fd0df03d4dc024310
redox7500 <redox7500@gmail.com>
2026-09-18 17:20:02 -0700

    Makes the test harness a little better

--- project1lists/TestHarness.java
+++ project1lists/TestHarness.java
@@ -36,17 +36,20 @@ public class TestHarness
         false
     };
 
-    /** Return type of testCustomListMethod */
-    private static enum TestCustomListResult
+    /** Return type of testErrors */
+    private static enum ErrorTestResult
     {
-        /** Returned by testCustomListMethod when customList and list having the same behavior and neither produce errors */
-        SUCCESS,
+        /** Returned by testErrors when neither state.customList nor state.arrayList produce errors */
+        NONE,
 
-        /** Returned by testCustomListMethod when customList and list have different behavior */
-        FAIL,
+        /** Returned by testErrors when both state.customList and state.arrayList produce an error (such as the current index being out of bounds) */
+        BOTH,
 
-        /** Returned by testCustomListMethod when both customList and list produce an error (such as the current index being out of bounds) */
-        ERROR
+        /** Returned by testErrors when state.customList produces an error but state.arrayList does not */
+        CUSTOM_LIST,
+
+        /** Returned by testErrors when state.arrayList produces an error but state.customList does not */
+        ARRAY_LIST,
     }
 
     /** Tests if customList and list behave the same when the specified function is called
@@ -57,9 +60,9 @@ public class TestHarness
      * @param currentIndex A supplementary property of list that matches your customList's internal current index (as stated before, both instances should have the exact same values/state)
      * @param function The index of which function you would like to test. These indices are 0 based and go from the top to the bottom of the CustomList.java file.
      * @param functionInput (optional) The value to give to the function you chose, which will not be used if the function you chose has no inputs
-     * @return A TestCustomListResult stating the behavior of the customList relative to the list
+     * @return A ErrorTestResult stating the behavior of the customList relative to the list
      */
-    private static TestCustomListResult testCustomListMethod(State<?> state)
+    private static ErrorTestResult testErrors(State<?> state)
     {
         int function = state.functionHistory[state.functionHistory.length - 1];
 
@@ -70,126 +73,124 @@ public class TestHarness
         {
             case 0:
             {
-                int customListSize;
-                try {customListSize = state.customList.size();}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                try {state.customList.size();}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
 
-                return (customListSize == state.arrayList.size())? TestCustomListResult.SUCCESS : TestCustomListResult.FAIL;
+                return ErrorTestResult.NONE;
             }
             case 1:
             {
-                int customListCurrentIndex;
-                try {customListCurrentIndex = state.customList.getCurrentIndex();}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                try {state.customList.getCurrentIndex();}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 
-                return (customListCurrentIndex == state.arrayListCurrentIndex)? TestCustomListResult.SUCCESS : TestCustomListResult.FAIL;
+                return ErrorTestResult.NONE;
             }
             case 2:
             {
-                boolean listError = argument < 0 || ((state.arrayList.size() > 0)? argument >= state.arrayList.size() : argument != 0);
+                boolean arrayListError = argument < 0 || ((state.arrayList.size() > 0)? argument >= state.arrayList.size() : argument != 0);
 
                 try {state.customList.moveCurrentIndexTo(argument);}
-                catch (Throwable _) {return (listError)? TestCustomListResult.ERROR : TestCustomListResult.FAIL;}
-                if (listError) return TestCustomListResult.FAIL;
+                catch (Throwable _) {return (arrayListError)? ErrorTestResult.BOTH : ErrorTestResult.CUSTOM_LIST;}
+                if (arrayListError) return ErrorTestResult.ARRAY_LIST;
                 state.arrayListCurrentIndex = argument;
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 3:
             {
                 try {state.customList.moveCurrentIndexToStart();}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 state.arrayListCurrentIndex = 0;
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 4:
             {
                 try {state.customList.moveCurrentIndexToEnd();}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 state.arrayListCurrentIndex = Math.max(state.arrayList.size() - 1, 0);
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 5:
             {
-                boolean listError = state.arrayListCurrentIndex <= 0;
+                boolean arrayListError = state.arrayListCurrentIndex <= 0;
 
                 try {state.customList.moveCurrentIndexLeft();}
-                catch (Throwable _) {return (listError)? TestCustomListResult.ERROR : TestCustomListResult.FAIL;}
+                catch (Throwable _) {return (arrayListError)? ErrorTestResult.BOTH : ErrorTestResult.CUSTOM_LIST;}
+                if (arrayListError) return ErrorTestResult.ARRAY_LIST;
                 state.arrayListCurrentIndex--;
 
-                return (listError)? TestCustomListResult.FAIL : TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 6:
             {
-                boolean listError = (state.arrayList.size() > 0)? state.arrayListCurrentIndex >= state.arrayList.size() - 1 : true;
+                boolean arrayListError = (state.arrayList.size() > 0)? state.arrayListCurrentIndex >= state.arrayList.size() - 1 : true;
 
                 try {state.customList.moveCurrentIndexRight();}
-                catch (Throwable _) {return (listError)? TestCustomListResult.ERROR : TestCustomListResult.FAIL;}
-                if (listError) return TestCustomListResult.FAIL;
+                catch (Throwable _) {return (arrayListError)? ErrorTestResult.BOTH : ErrorTestResult.CUSTOM_LIST;}
+                if (arrayListError) return ErrorTestResult.ARRAY_LIST;
                 state.arrayListCurrentIndex++;
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 7:
             {
-                boolean listError = state.arrayList.size() == 0;
+                boolean arrayListError = state.arrayList.size() == 0;
 
-                Integer customListCurrentValue;
-                try {customListCurrentValue = state.customList.getCurrentValue();}
-                catch (Throwable _) {return (listError)? TestCustomListResult.ERROR : TestCustomListResult.FAIL;}
-                if (listError) return TestCustomListResult.FAIL;
+                try {state.customList.getCurrentValue();}
+                catch (Throwable _) {return (arrayListError)? ErrorTestResult.BOTH : ErrorTestResult.CUSTOM_LIST;}
+                if (arrayListError) return ErrorTestResult.ARRAY_LIST;
 
-                return (customListCurrentValue == state.arrayList.get(state.arrayListCurrentIndex))? TestCustomListResult.SUCCESS : TestCustomListResult.FAIL;
+                return ErrorTestResult.NONE;
             }
             case 8:
             {
                 try {state.customList.setCurrentValue(argument);}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 if (state.arrayList.size() > 0) state.arrayList.set(state.arrayListCurrentIndex, argument);
                 else state.arrayList.add(argument);
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 9:
             {
                 try {state.customList.clear();}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 state.arrayList.clear();
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 10:
             {
                 try {state.customList.insert(argument);}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 state.arrayList.add(state.arrayListCurrentIndex, argument);
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 11:
             {
                 try {state.customList.append(argument);}
-                catch (Throwable _) {return TestCustomListResult.FAIL;}
+                catch (Throwable _) {return ErrorTestResult.CUSTOM_LIST;}
                 state.arrayList.add(argument);
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             case 12:
             {
-                boolean listError = state.arrayList.size() == 0;
+                boolean arrayListError = state.arrayList.size() == 0;
 
                 try {state.customList.remove();}
-                catch (Throwable _) {return (listError)? TestCustomListResult.ERROR : TestCustomListResult.FAIL;}
-                if (listError) return TestCustomListResult.FAIL;
+                catch (Throwable _) {return (arrayListError)? ErrorTestResult.BOTH : ErrorTestResult.CUSTOM_LIST;}
+                if (arrayListError) return ErrorTestResult.ARRAY_LIST;
                 state.arrayList.remove(state.arrayListCurrentIndex);
 
-                return TestCustomListResult.SUCCESS;
+                return ErrorTestResult.NONE;
             }
             default:
             {
-                return TestCustomListResult.FAIL; // This code should never be able to happen because of the assert above
+                return ErrorTestResult.NONE; // This code should never be able to happen because of the assert above
             }
         }
     }
@@ -232,7 +233,11 @@ public class TestHarness
         {
             try
             {
-                if (customList.size() != arrayList.size() || customList.getCurrentIndex() != this.arrayListCurrentIndex) return false;
+                if (
+                    customList.size() != arrayList.size() ||
+                    customList.getCurrentIndex() != this.arrayListCurrentIndex ||
+                    (customList.size() > 0 && arrayList.size() > 0 && customList.getCurrentValue() != this.arrayList.get(this.arrayListCurrentIndex))
+                ) return false;
 
                 int customListIndex = customList.getCurrentIndex();
                 
@@ -261,6 +266,17 @@ public class TestHarness
             try {System.out.println(this.customList.getCurrentIndex());}
             catch (Throwable error) {error.printStackTrace();}
             System.out.println("ArrayList current index:   " + this.arrayListCurrentIndex);
+            System.out.print("Custom list current value: ");
+            try
+            {
+                if (this.customList.size() > 0) System.out.println(this.customList.getCurrentValue());
+                else System.out.println("N/A (empty list)");
+            }
+            catch (Throwable error)
+            {
+                error.printStackTrace();
+            }
+            System.out.println("ArrayList current value:   " + ((this.arrayList.size() > 0)? this.arrayList.get(this.arrayListCurrentIndex) : "N/A (empty list)"));
             System.out.println("Function history (most recent at the bottom): ");
             for (int i = 0; i < this.functionHistory.length; i++)
             {
@@ -292,16 +308,18 @@ public class TestHarness
                 {
                     State<T> newState = new State<>(this, currentFunction, currentArgument);
                     
-                    switch (TestHarness.testCustomListMethod(newState))
+                    switch (TestHarness.testErrors(newState))
                     {
-                        case TestCustomListResult.ERROR:
+                        case ErrorTestResult.BOTH:
                             continue;
-                        case TestCustomListResult.FAIL:
-                            System.out.println("Failed at state below");
+                        case ErrorTestResult.CUSTOM_LIST:
+                            System.out.println("Custom list had an error while ArrayList did not at state below");
                             newState.printInfo();
 
                             return false;
-                        case TestCustomListResult.SUCCESS:
+                        case ErrorTestResult.ARRAY_LIST:
+                            System.out.println("ArrayList had an error while custom list did not at state below");
+                        case ErrorTestResult.NONE:
                             if (!newState.isValid())
                             {
                                 System.out.println("Lists were not equal at state below");
@@ -319,7 +337,7 @@ public class TestHarness
     }
 
     /** Test every method of the customListClass
-     * Note: This function also only works with an element type of Integer, see {@link project1lists.TestHarness.testCustomListMethod(CustomList, List, int, int, int) testCustomListMethod} for why
+     * Note: This function also only works with an element type of Integer, see {@link project1lists.TestHarness.testErrors(CustomList, List, int, int, int) testErrors} for why
      * @param customListClass The class to test the behavior of
      * @param depth How many layers deep every function is applied (making a new layer) and the behavior tested
      * @return A boolean representing whether or not the customListClass behaves as expected (compared to the builtin {@link java.util.ArrayList ArrayList}) <!-- this link doesn't work for some reason -->
