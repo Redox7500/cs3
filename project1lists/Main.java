eddd0ac9b4739fc501131d94a54ea2eb24289e3a
redox7500 <redox7500@gmail.com>
2026-09-20 20:13:02 -0700

    Moves the testing stuff from Main to TestHarness where it probably belongs

--- project1lists/Main.java
+++ project1lists/Main.java
@@ -1,93 +1,19 @@
 package project1lists;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.lang.InterruptedException;
 import java.util.function.Supplier;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.print("\033[?25l");
-
-        @SuppressWarnings("rawtypes") // i think that this is where it's supposed to go instead of at the top, i'll change that in the other files later
-        Supplier[] constructors = new Supplier<?>[]{
+        TestHarness.testCustomLists(new Supplier<?>[]{
             () -> new CustomArrayList       <Integer>(),
             () -> new CustomLinkedList      <Integer>(),
             () -> new CustomDoublyLinkedList<Integer>()
-        };
-        String[] names = new String[]{
+        }, new String[]{
             "CustomArrayList",
             "CustomLinkedList",
             "CustomDoublyLinkedList"
-        };
-        int maxNameLength = 0;
-        for (String name : names) maxNameLength = Math.max(maxNameLength, name.length());
-        for (String name : names) System.out.println(String.format("%-" + (maxNameLength + 2) + "s", name + ": ") + "...");
-
-        System.out.print("\033[3A");
-        AsyncSpinner.start();
-        for (int i = 0; i < names.length; i++)
-        {
-            System.out.print("\033[25G   \033[25G");
-            AsyncSpinner.start();
-
-            long startTime = System.nanoTime();
-            @SuppressWarnings("unchecked")
-            boolean result = TestHarness.testCustomList(constructors[i], 7);
-            float elapsedTime = (System.nanoTime() - startTime) / (float)1_000_000_000;
-
-            AsyncSpinner.stop();
-            if (result)
-            {
-                System.out.println("Success (" + elapsedTime + " seconds elapsed)");
-            }
-            else
-            {
-                System.out.println("\033[" + i + "A (" + elapsedTime + " seconds elapsed)");
-                break;
-            }
-        }
-        AsyncSpinner.stop();
-        
-        // System.out.println("CustomArrayList:        ...\nCustomLinkedList:       ...\nCustomDoublyLinkedList: ...");
-        // boolean arrayListResult = TestHarness.testCustomList(() -> new CustomArrayList<Integer>(), 7);
-        // System.out.println("\033[3A\033[25G" + ((TestHarness.testCustomList(() -> new CustomArrayList<Integer>(),        7))? "Success" : "")); // warning: depth 8 gave me a java heap space error with all of the custom list kinds
-        // System.out.println("\033[25G" + ((TestHarness.testCustomList(() -> new CustomLinkedList<Integer>(),       7))? "Success" : ""));
-        // System.out.println("\033[25G" + ((TestHarness.testCustomList(() -> new CustomDoublyLinkedList<Integer>(), 7))? "Success" : ""));
-        // System.out.println("\nElapsed time: " + ((System.nanoTime() - startTime) / (float)1_000_000_000));
-        AsyncSpinner.kill();
-        System.out.print("\033[99999;99999H\033[1A\033[?25h");
-    }
-
-    private static class AsyncSpinner
-    {
-        static final char[] frames = {'|', '/', '-', '\\'};
-        static ExecutorService executor;
-        static Future<?> task;
-
-        static void animate()
-        {
-            int frame = 0;
-            while (!Thread.currentThread().isInterrupted())
-            {
-                System.out.print(frames[frame] + "\033[1D");
-                frame = (frame + 1) % frames.length;
-                try {Thread.sleep(100);}
-                catch (InterruptedException _) {Thread.currentThread().interrupt();}
-            }
-        }
-
-        static void start()
-        {
-            if (AsyncSpinner.executor == null) AsyncSpinner.executor = Executors.newSingleThreadExecutor();
-            AsyncSpinner.task = executor.submit(AsyncSpinner::animate);
-        }
-
-        static void stop() {if (AsyncSpinner.task != null) AsyncSpinner.task.cancel(true);}
-
-        static void kill() {if (AsyncSpinner.executor != null) AsyncSpinner.executor.shutdownNow();}
+        });
     }
 }
--- project1lists/TestHarness.java
+++ project1lists/TestHarness.java
@@ -6,6 +6,9 @@ import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.Consumer;
 import java.util.function.BiConsumer;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.Executors;
 
 public class TestHarness
 {
@@ -25,13 +28,65 @@ public class TestHarness
         new TestHarness.CustomListMethod.CustomListValueSupplier<>("remove",                  CustomList::remove)
     };
 
+    @SuppressWarnings("rawtypes") // i think that this is where it's supposed to go instead of at the top, i'll change that in the other files later
+    public static void testCustomLists(Supplier[] constructors, String[] names)
+    {
+        System.out.print("\033[?25l");
+
+        // Supplier[] constructors = new Supplier<?>[]{
+        //     () -> new CustomArrayList       <Integer>(),
+        //     () -> new CustomLinkedList      <Integer>(),
+        //     () -> new CustomDoublyLinkedList<Integer>()
+        // };
+        // String[] names = new String[]{
+        //     "CustomArrayList",
+        //     "CustomLinkedList",
+        //     "CustomDoublyLinkedList"
+        // };
+
+        int customListCount = constructors.length;
+        if (customListCount != names.length) return;
+
+        int maxNameLength = 0;
+        for (String name : names) maxNameLength = Math.max(maxNameLength, name.length());
+        for (String name : names) System.out.println(String.format("%-" + (maxNameLength + 2) + "s", name + ": ") + "-");
+
+        System.out.print("\033[" + customListCount + "A");
+        AsyncSpinner.start();
+
+        for (int i = 0; i < customListCount; i++)
+        {
+            System.out.print("\033[25G\033[25G");
+            AsyncSpinner.start();
+
+            long startTime = System.nanoTime();
+            @SuppressWarnings("unchecked")
+            boolean result = TestHarness.testCustomList(constructors[i], 7);
+            String elapsedTime = String.format("%.5f", (System.nanoTime() - startTime) / (float)1_000_000_000);
+
+            AsyncSpinner.stop();
+            if (result)
+            {
+                System.out.println("Success (" + elapsedTime + " seconds elapsed)");
+            }
+            else
+            {
+                System.out.println("\033[" + i + "A (" + elapsedTime + " seconds elapsed)");
+                break;
+            }
+        }
+
+        AsyncSpinner.kill();
+        System.out.print("\033[99999;99999H\033[1A\033[?25h");
+    }
+
     /** Test every method of the customListClass
      * Note: This function also only works with an element type of Integer, see {@link project1lists.TestHarness.testErrors(CustomList, List, int, int, int) testErrors} for why
      * @param customListClass The class to test the behavior of
      * @param depth How many layers deep every function is applied (making a new layer) and the behavior tested
      * @return A boolean representing whether or not the customListClass behaves as expected (compared to the builtin {@link java.util.ArrayList ArrayList}) <!-- this link doesn't work for some reason -->
      */
-    public static <T extends CustomList<Integer, T>> boolean testCustomList(Supplier<T> constructor, int depth)
+    private static <T extends CustomList<Integer, T>> boolean testCustomList(Supplier<T> constructor, int depth)
     {
         ArrayDeque<State<T>> leaves = new ArrayDeque<>();
         leaves.add(new State<>(constructor));
@@ -341,6 +396,35 @@ public class TestHarness
             INVALID,
             ERROR
         }
+    }
+
+    private static class AsyncSpinner
+    {
+        static final char[] frames = {'-', '\\', '|', '/'};
+        static ExecutorService executor;
+        static Future<?> task;
+
+        static void animate()
+        {
+            int frame = 0;
+            while (!Thread.currentThread().isInterrupted())
+            {
+                System.out.print(frames[frame] + "\033[1D");
+                frame = (frame + 1) % frames.length;
+                try {Thread.sleep(100);}
+                catch (InterruptedException _) {Thread.currentThread().interrupt();}
+            }
+        }
+
+        static void start()
+        {
+            if (AsyncSpinner.executor == null) AsyncSpinner.executor = Executors.newSingleThreadExecutor();
+            AsyncSpinner.task = executor.submit(AsyncSpinner::animate);
+        }
+
+        static void stop() {if (AsyncSpinner.task != null) AsyncSpinner.task.cancel(true);}
+
+        static void kill() {if (AsyncSpinner.executor != null) AsyncSpinner.executor.shutdownNow();}
     }
 
     private sealed interface CustomListMethod
